2d331e33e82c67668a498cd0838d2bf63b9c78f8
ttxs <11433256@qq.com>
2025-06-04 17:28:14 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://api.888727.xyz:21114".to_owned()
 }
 
 #[inline]
